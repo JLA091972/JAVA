d137fc1a9016f9cdf91724bb0edbd7f139c65fad
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-03-06 14:20:22 -0700

    My First Java program

---
+++ FirstJavaProgram/firstJavaProgram.java
@@ -0,0 +1,8 @@
+
+public class firstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }    
+}
